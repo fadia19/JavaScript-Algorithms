47917810c6761bb89171d99297dcaf41001f971b
Fadia Tarek <fadia.tarek19@gmail.com>
2018-06-07 21:17:03 +0200

    Add files via upload

---
+++ harmlessRansome.js
@@ -0,0 +1,24 @@
+function harmlessRansomNote(noteText, magazineText) {
+	var noteArr = noteText.split(' ');
+	var magazineArr = magazineText.split(' ');
+	var magazineObj = {};
+
+	magazineArr.forEach(word => {
+		if (!magazineObj[word]) magazineObj[word] = 0;
+		magazineObj[word]++;
+	});
+
+	var noteExists = true;
+	noteArr.forEach(word => {
+		if (magazineObj[word]) {
+			magazineObj[word]--;
+      if(magazineObj[word]<0) noteExists=false;
+		} else {
+			noteExists = false;
+		} 
+	});
+
+  return noteExists;
+}
+
+harmlessRansomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall location that i am an admirer of you must hike quite a distance to find the secret places as they are far from the populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places that i have ever visited');
