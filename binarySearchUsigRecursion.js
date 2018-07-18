c1a5ff19f66e8560c3f4e920df6982fd4b501119
Fadia Tarek <fadia.tarek19@gmail.com>
2018-07-18 03:20:45 +0200

    Add files via upload

---
+++ binarySearchUsigRecursion.js
@@ -0,0 +1,19 @@
+function binarySearch(numArray, key) {
+  var middleIndex = Math.floor(numArray.length/2);
+  var middleElem = numArray[middleIndex];
+
+  if(middleElem === key){
+    return true;
+  }
+  else if(middleElem < key && numArray.length > 1) {
+    return binarySearch(numArray.splice(middleIndex , numArray.length) , key);        
+    }
+  else if(middleElem > key && numArray.length >1){
+    return binarySearch(numArray.splice(0 , middleIndex) , key);
+  }
+  else {
+    return false;
+  }
+  }
+
+binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 20);
