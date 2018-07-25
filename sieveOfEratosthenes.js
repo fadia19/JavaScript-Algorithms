7466e8398d8c0ccb80fed707d9dec430011ee6a0
Fadia Tarek <fadia.tarek19@gmail.com>
2018-07-25 14:21:26 +0200

    Add files via upload

---
+++ sieveOfEratosthenes.js
@@ -0,0 +1,23 @@
+// the algorithm takes a number and returns an array with all prime numbers up to this number
+function sieveOfEratosthenes(num) {
+	var primes = [];
+	for (var i = 0; i <= num; i++) {
+		primes[i] = true;
+	}
+	primes[0] = false;
+	primes[1] = false;
+
+	for (var i = 2; i <= Math.sqrt(num); i++) {
+		for (j = 2; j * i <= num; j++) {
+			primes[i * j] = false;
+		}
+	}
+
+	var result = [];
+	for (var i = 0; i < primes.length; i++) {
+		if (primes[i]) result.push(i);
+	}
+
+	return result;
+}
+sieveOfEratosthenes(200);
